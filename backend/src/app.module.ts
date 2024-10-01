5a9983b4c6d61e24e46e2d8d9cb84e841f527710
Timur <timur1776@gmail.com>
2024-10-01 13:04:53 +0300

    init chatgpt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TelegramModule } from './telegram/telegram.module';
+import { ChatgptModule } from './chatgpt/chatgpt.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TelegramModule],
-  controllers: [],
-  providers: [],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), TelegramModule, ChatgptModule],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
