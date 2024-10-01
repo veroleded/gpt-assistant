b42dc34220cfc79a393a6e286c8b7ec61e706edb
Timur <timur1776@gmail.com>
2024-10-01 14:43:26 +0300

    add: start gpt module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TelegramModule } from './telegram/telegram.module';
 import { ChatgptModule } from './chatgpt/chatgpt.module';
+import { FsModule } from './fs/fs.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), TelegramModule, ChatgptModule],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), TelegramModule, ChatgptModule, FsModule],
     controllers: [],
     providers: [],
 })
--- backend/src/chatgpt/chatgpt.service.ts
+++ backend/src/chatgpt/chatgpt.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class ChatgptService {}
+export class ChatgptService {
+    private readonly logger = new Logger(ChatgptService.name);
+    private gptUrl = '';
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly httpService: HttpService,
+    ) {
+        this.gptUrl = this.configService.get('PROXY_OPENAi_URL');
+    }
+}
