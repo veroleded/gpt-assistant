3b93ecef1509183d2328820813be27ba5dcf76a1
Timur Kharisov <timur1776@gmail.com>
2024-10-02 19:19:10 +0300

    add: all gpt models naming

---
+++ backend/src/chatgpt/models.ts
@@ -0,0 +1,17 @@
+export const models = {
+  gpt4o: 'gpt-4o',
+  gpt4turbo: 'gpt-4-turbo',
+  gpt4: 'gpt4',
+  gpt35turbo: 'gpt-3.5-turbo',
+  textEmbedding3Small: 'text-embedding-3-small',
+  textEmbedding3Large: 'text-embedding-3-large',
+
+  textEmbeddingAda002: 'text-embedding-ada-002',
+  dalle2: 'dall-e-2',
+  dalle3: 'dall-e-3',
+  whisper1: 'whisper-1',
+  tts1: 'tts-1',
+  tts1hd: 'tts-1-hd',
+  
+
+};
