d7f2cec3742dfbac27ac8af87b59bfcceac8beca
Timur Kharisov <timur1776@gmail.com>
2024-10-03 15:34:13 +0300

    add: 20 message limit

--- backend/src/telegram/telegram.service.ts
+++ backend/src/telegram/telegram.service.ts
@@ -1,18 +1,13 @@
 import { ConfigService } from '@nestjs/config';
 import { Role } from '@prisma/client';
-import { Action, Command, Ctx, Message, On, Start, Update } from 'nestjs-telegraf';
-import { ChatCompletionUserMessageParam } from 'openai/resources';
+import { Command, Ctx, Message, On, Start, Update } from 'nestjs-telegraf';
 import { ChatgptService } from 'src/chatgpt/chatgpt.service';
-import { models } from 'src/chatgpt/models';
 import { FilesService } from 'src/files/files.service';
 import { MessageService } from 'src/message/message.service';
-import { CreateSessionDto } from 'src/session/dto/create-session.dto';
 import { SessionService } from 'src/session/session.service';
 import { UserService } from 'src/user/user.service';
-import { interTagCode } from 'src/utils/interTagCode';
 
 import { Telegraf } from 'telegraf';
-import { code } from 'telegraf/format';
 import { SceneContext } from 'telegraf/typings/scenes';
 
 interface Context extends SceneContext { }
@@ -58,6 +53,12 @@ export class TelegramService extends Telegraf<Context> {
 
             const session = await this.sessionService.findCurrentUserSession(userId);
             const messages = await this.messageService.findAllSessionMessages(session.id);
+
+            if (messages.length >= 20) {
+                return ctx.reply('Сообщения в этом чате закончились, используйте команду /new что бы начать новый');
+
+            }
+
             const newMessage = { role: Role.user, content: text };
             messages.push(newMessage);
 
@@ -83,14 +84,18 @@ export class TelegramService extends Telegraf<Context> {
     async onVoice(@Ctx() ctx: Context, @Message('voice') voice: any) {
         try {
             const userId = ctx.message.from.id;
+            const session = await this.sessionService.findCurrentUserSession(userId);
+            const messages = await this.messageService.findAllSessionMessages(session.id);
+            if (messages.length >= 20) {
+                return ctx.reply('Сообщения в этом чате закончились, используйте команду /new что бы начать новый');
+
+            }
+
             const fileLink = await ctx.telegram.getFileLink(voice.file_id);
             const filepath = await this.filesService.downloadFile(fileLink.href, userId.toString(), 'ogg');
             const transcription = await this.chatgptService.transcription(filepath);
 
             const newMessage = { role: Role.user, content: transcription };
-
-            const session = await this.sessionService.findCurrentUserSession(userId);
-            const messages = await this.messageService.findAllSessionMessages(session.id);
             messages.push(newMessage);
 
             const response = await this.chatgptService.generateTextResponse(messages);
