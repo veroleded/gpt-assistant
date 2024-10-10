980536b458d68b5de8f2ca9f80d1a750c9963689
Timur Kharisov <timur1776@gmail.com>
2024-10-10 14:10:12 +0300

    fix: deploy errors

--- backend/src/modules/telegram/telegram.module.ts
+++ backend/src/modules/telegram/telegram.module.ts
@@ -6,12 +6,12 @@ import { ChatgptModule } from 'src/modules/chatgpt/chatgpt.module';
 import { UserModule } from 'src/modules/user/user.module';
 import { SessionModule } from 'src/modules/session/session.module';
 import { MessageModule } from 'src/modules/message/message.module';
-import { StartScene } from './scenes/start.scene.js';
-import { MenuScene } from './scenes/menu.scene.js';
-import { SetContextScene } from './scenes/set-context.scene.js';
-import { GptScene } from './scenes/gpt.scene.js';
 import { FilesModule } from 'src/libs/files/files.module';
 import { BalanceModule } from 'src/libs/balance/balance.module';
+import { StartScene } from './scenes/start.scene';
+import { MenuScene } from './scenes/menu.scene';
+import { SetContextScene } from './scenes/set-context.scene';
+import { GptScene } from './scenes/gpt.scene';
 
 @Module({
     imports: [
@@ -21,7 +21,7 @@ import { BalanceModule } from 'src/libs/balance/balance.module';
         UserModule,
         SessionModule,
         MessageModule,
-        BalanceModule,
+        BalanceModule
     ],
     providers: [TelegramService, StartScene, MenuScene, SetContextScene, GptScene],
 })
