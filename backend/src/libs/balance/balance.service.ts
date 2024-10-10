ae3e481984583effdd18e2dad37ef1337ea495d9
Timur Kharisov <timur1776@gmail.com>
2024-10-10 14:05:32 +0300

    fix: deploy errors

--- backend/src/libs/balance/balance.service.ts
+++ backend/src/libs/balance/balance.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
-
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Injectable()
 export class BalanceService {
+    constructor(
+        private readonly configService: ConfigService,
+    ) {}
+
+    async getBalance() {
+        const response = await axios(this.configService.get('BALANCE_URL'), {
+            method: 'GET',
+            headers: { Authorization: this.configService.get('OPENAI_API_KEY') },
+            
+        });
+
+        return response.data.balance;
+    }
 }
--- backend/src/modules/telegram/telegram.module.ts
+++ backend/src/modules/telegram/telegram.module.ts
@@ -6,11 +6,12 @@ import { ChatgptModule } from 'src/modules/chatgpt/chatgpt.module';
 import { UserModule } from 'src/modules/user/user.module';
 import { SessionModule } from 'src/modules/session/session.module';
 import { MessageModule } from 'src/modules/message/message.module';
-import { StartScene } from './scenes/start.scene';
-import { MenuScene } from './scenes/menu.scene';
-import { SetContextScene } from './scenes/set-context.scene';
-import { GptScene } from './scenes/gpt.scene';
+import { StartScene } from './scenes/start.scene.js';
+import { MenuScene } from './scenes/menu.scene.js';
+import { SetContextScene } from './scenes/set-context.scene.js';
+import { GptScene } from './scenes/gpt.scene.js';
 import { FilesModule } from 'src/libs/files/files.module';
+import { BalanceModule } from 'src/libs/balance/balance.module';
 
 @Module({
     imports: [
@@ -20,6 +21,7 @@ import { FilesModule } from 'src/libs/files/files.module';
         UserModule,
         SessionModule,
         MessageModule,
+        BalanceModule,
     ],
     providers: [TelegramService, StartScene, MenuScene, SetContextScene, GptScene],
 })
