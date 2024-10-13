5dcc1443b0dcf40c4b9231b921f45c4785c4ec99
Timur <timur1776@gmail.com>
2024-10-13 20:39:40 +0300

    add: voice add context

--- backend/src/modules/telegram/scenes/set-context.scene.ts
+++ backend/src/modules/telegram/scenes/set-context.scene.ts
@@ -2,18 +2,23 @@ import { Ctx, Message, On, Scene, SceneEnter } from 'nestjs-telegraf';
 import { SceneContext } from 'telegraf/typings/scenes';
 import { SessionService } from 'src/modules/session/session.service';
 import { UserService } from 'src/modules/user/user.service';
+import { FilesService } from 'src/libs/files/files.service';
+import { ChatgptService } from 'src/modules/chatgpt/chatgpt.service';
 
 @Scene('set_context')
 export class SetContextScene {
     constructor(
         private readonly sessionService: SessionService,
         private readonly userService: UserService,
+        private readonly filesService: FilesService,
+        private readonly chatgptService: ChatgptService,
     ) {}
 
     @SceneEnter()
     async enter(@Ctx() ctx: SceneContext) {
         ctx.editMessageText(
-            'Опишите как должен вести себя бот. Пример описания:\n' + 'Отвечай как будто ты великий математик.',
+            'Опишите текстом или голосовым сообщением как должен вести себя бот. Пример описания:\n' +
+                'Отвечай как будто ты великий математик.',
         );
     }
 
@@ -22,6 +27,21 @@ export class SetContextScene {
         const { id } = ctx.message.from;
         const session = await this.sessionService.findCurrentUserSession(id.toString());
         await this.sessionService.update(session.id, { context: text });
-        await ctx.scene.enter('menu');
+
+        await ctx.reply('Сохранено!');
+        await ctx.scene.enter('settings');
+    }
+
+    @On('voice')
+    async onVoice(@Ctx() ctx: SceneContext, @Message('voice') voice: any) {
+        const { id } = ctx.message.from;
+        const fileLink = await ctx.telegram.getFileLink(voice.file_id);
+        const filepath = await this.filesService.downloadFile(fileLink.href, id.toString(), 'ogg');
+        const transcription = await this.chatgptService.transcription(filepath);
+        const session = await this.sessionService.findCurrentUserSession(id.toString());
+        await this.sessionService.update(session.id, { context: transcription });
+
+        await ctx.reply('Сохранено!');
+        await ctx.scene.enter('setttings');
     }
 }
